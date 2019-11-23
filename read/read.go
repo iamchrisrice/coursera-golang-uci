66ccbb176a10514bf52431e9531f098ccb3d234c
Chris Rice <chrisriceuk@gmail.com>
2019-11-23 09:07:03 +0000

    Second Week 4 coursework

---
+++ read/read.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+type name struct {
+	fname string
+	lname string
+}
+
+func stringToName(s string) name {
+	w := strings.Split(s, " ")
+	n := name{fname: w[0], lname: w[1]}
+	return n
+}
+
+func nameToString(n name) string {
+	return "First Name: " + n.fname + ", Last Name: " + n.lname
+}
+
+func main() {
+	stdin := bufio.NewScanner(os.Stdin)
+
+	fmt.Print("Filename: ")
+	stdin.Scan()
+
+	file, err := os.Open(stdin.Text())
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	names := make([]name, 0, 1)
+
+	for scanner.Scan() {
+		names = append(names, stringToName(scanner.Text()))
+	}
+
+	for _, n := range names {
+		fmt.Println(nameToString(n))
+	}
+}
