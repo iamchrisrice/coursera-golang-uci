8c5f16e2165a84b6302fb6de916df0c5bb1bf66a
Chris Rice <chrisriceuk@gmail.com>
2019-11-24 20:32:52 +0000

    Week 7 coursework

---
+++ animal/animal.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+type Animal struct {
+	food       string
+	locomotion string
+	noise      string
+}
+
+func (a Animal) Eat() {
+	fmt.Println(a.food)
+}
+
+func (a Animal) Move() {
+	fmt.Println(a.locomotion)
+}
+
+func (a Animal) Speak() {
+	fmt.Println(a.noise)
+}
+
+func getAnimalAndCommandFromInput(input string) (string, string) {
+	w := strings.Split(input, " ")
+	animal := strings.ToLower(w[0])
+	command := strings.ToLower(w[1])
+	return animal, command
+}
+
+func animalCommand(animal Animal, command string) {
+	switch command {
+	case "eat":
+		animal.Eat()
+	case "move":
+		animal.Move()
+	case "speak":
+		animal.Speak()
+	}
+}
+
+func createAnimals() map[string]Animal {
+	animals := make(map[string]Animal)
+	animals["cow"] = Animal{"grass", "walk", "moo"}
+	animals["bird"] = Animal{"worms", "fly", "peep"}
+	animals["snake"] = Animal{"mice", "slither", "hsss"}
+	return animals
+}
+
+func main() {
+	animals := createAnimals()
+
+	for {
+		fmt.Print("> ")
+
+		stdin := bufio.NewScanner(os.Stdin)
+		stdin.Scan()
+		input := stdin.Text()
+
+		animal, command := getAnimalAndCommandFromInput(input)
+		if _, exists := animals[animal]; exists {
+			animalCommand(animals[animal], command)
+		}
+	}
+}
