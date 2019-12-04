9278c6ed3496a3b64cc486e197bc616a4e6e4aba
Chris Rice <chrisriceuk@gmail.com>
2019-12-04 04:48:25 -0800

    Week 9 coursework

---
+++ race-condition/race-condition.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+
+	result := make([]string, 0, 3)
+
+	/**
+	 Two goroutines appending to same slice.
+	 Order of slice at end of program execution, and therefore printed output, depends on order of goroutine execution.
+	**/
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		result = append(result, "Valtteri Bottas")
+	}()
+	go func() {
+		defer wg.Done()
+		result = append(result, "Lewis Hamilton")
+	}()
+	wg.Wait()
+
+	fmt.Println("The winner is: " + result[0])
+
+}
